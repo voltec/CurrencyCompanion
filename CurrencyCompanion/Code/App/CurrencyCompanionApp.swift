3208dbf324111c16f037ba049f812cd855fd01cb
Mikhail Mukminov <voltec366@gmail.com>
2024-02-17 21:46:20 +0400

    feat: init base structure

---
+++ CurrencyCompanion/Code/App/CurrencyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCompanionApp.swift
+//  CurrencyCompanion
+//
+//  Created by Mikhail Mukminov on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+          CurrencyConversionView()
+        }
+    }
+}
---
+++ CurrencyCompanion/Code/Presentation/CurrencyConversionView/CurrencyConversionView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyConversionView.swift
+//  CurrencyCompanion
+//
+//  Created by Mikhail Mukminov on 17.02.2024.
+//
+
+import SwiftUI
+
+struct CurrencyConversionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyConversionView()
+}
